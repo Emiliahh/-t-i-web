37bbf14de6754426e30e039bccf8bfa759c8b949
fak_you <huynguyen.nh99@gmail.com>
2024-04-05 23:52:28 +0700

    linh tinh x2

--- function.js
+++ function.js
@@ -1,5 +1,21 @@
-function login(){
-    let id = prompt("User name");
-    let pw= prompt("Password")
-    
+window.addEventListener("scroll", function () {
+    let navbar = document.getElementById("navi-head");
+    if (window.pageYOffset > 50) {
+        console.log(window.pageYOffset);
+        navbar.classList.add("navi-head-fixed");
+    }
+    else {
+        navbar.classList.remove("navi-head-fixed");
+    }
+
+})
+function sideshow(){
+    const side=document.querySelector('.side-bar');
+    console.log("runing")
+    side.style.display="flex";
+}
+function sideclose(){
+    const side=document.querySelector('.side-bar');
+    console.log("runing")
+    side.style.display="none";
 }
