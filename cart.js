24f871bc807fd2d7c12f4a8d30b30219e015575b
fak_you <huynguyen.nh99@gmail.com>
2024-04-06 15:41:24 +0700

    code it thoi

---
+++ cart.js
@@ -0,0 +1,82 @@
+ products = [
+  {
+    id: 1,
+    name: "Áo kiểu nữ cam đất phối cổ trắng dập ly",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, velit.",
+    price: 250000,
+    image: "clothes/1.jpg",
+    count: 1,
+  },
+  {
+    id: 2,
+    name: "Áo trắng bèo lé đen tay loe dễ thương",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, velit.",
+    price: 350000,
+    image: "clothes/2.jpg",
+    count: 1,
+  },
+  {
+    id: 3,
+    name: "Áo trắng bèo lé đen tay loe dễ thương",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, velit.",
+    price: 350000,
+    image: "clothes/3.jpg",
+    count: 1,
+  },
+  {
+    id: 4,
+    name: "Áo trắng bèo lé đen tay loe dễ thương",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, velit.",
+    price: 350000,
+    image: "clothes/4.jpg",
+    count: 1,
+  },
+  {
+    id: 5,
+    name: "Áo trắng bèo lé đen tay loe dễ thương",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, velit.",
+    price: 350000,
+    image: "clothes/5.jpg",
+    count: 1,
+  },
+  {
+    id: 6,
+    name: "Áo trắng bèo lé đen tay loe dễ thương",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, velit.",
+    price: 350000,
+    image: "clothes/6.jpg",
+    count: 1,
+  },
+  {
+    id: 7,
+    name: "Áo trắng bèo lé đen tay loe dễ thương",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, velit.",
+    price: 350000,
+    image: "clothes/7.jpg",
+    count: 1,
+  },
+  {
+    id: 8,
+    name: "Áo trắng bèo lé đen tay loe dễ thương",
+    description:
+      "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, velit.",
+    price: 350000,
+    image: "clothes/8.jpg",
+    count: 1,
+  },
+];
+let prodcut_list =new Map()
+function add_list(index){
+  prodcut_map.set(products[index],i++);
+}
+function render(arr){
+  
+}
+
